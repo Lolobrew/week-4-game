a518b837283aa086c10d4164bdde479ca9dc8240
Jeff Schultz <Jeff Schultz>
2017-05-03 22:29:45 -0700

    commented out js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
-	
+
+//establish global variables outside of game loop
+
 	var randomNumber;
 
 	var total;
@@ -8,19 +10,23 @@ $(document).ready(function(){
 
 	var lose = 0;
 
-
+//set game loop
 	function resetGame(){
+
 		//assign a random number between 19 and 120
-		randomNumber = Math.floor(Math.random()*101) + 19;
+		randomNumber = Math.floor(Math.random()*120) + 19;
 		console.log(randomNumber);
 
+		//establish player total = 0 on game start
 		total = 0;
 
 		//set random_number div text to random number
 		$("#random_number").text(randomNumber);
 
+		//initialize text for user total score
 		$("#total_score").text(total);
 
+		//set each crystal with a random number between 1 and 12, respectively
 		$("#crystal1").attr("data", Math.floor(Math.random()*12) + 1);
 		$("#crystal2").attr("data", Math.floor(Math.random()*12) + 1);
 		$("#crystal3").attr("data", Math.floor(Math.random()*12) + 1);
@@ -28,23 +34,32 @@ $(document).ready(function(){
 	}
 
 
-
+//start of game loop, on click event to any crystal with a class of crystal
 $(".crystal").on("click", function(){
-		var crystalNumber = $(this).attr("data");
-		total += parseInt(crystalNumber);
-		console.log(total);
 
+	//establish new variable crystalNumber that takes on the data (random number) of the crystal clicked on
+		var crystalNumber = $(this).attr("data");
+
+	//add the crystalNumber var to the user's total var
+		total += parseInt(crystalNumber);
+
+		console.log(total);
 		console.log(crystalNumber);
+
+
+	//update the text of the user's total score
 		$("#total_score").text(total);
 
+		//if player score === the random number, it will alert game won, win counter increases, text updates, reset game funtion occurs
 		if(total === randomNumber){
-			alert ("Game Won");
+			alert ("Game Won!!");
 			wins++;
 			$("#win").text(wins);
 			resetGame();
 
+		//if player score > the random number, it will alert game lost, lose counter increases, text updates, reset game function occurs
 		} else if (total > randomNumber){
-			alert ("Game Lost");
+			alert ("Game Lost..");
 			lose++;
 			$("#lose").text(lose);
 			resetGame();
@@ -52,6 +67,8 @@ $(".crystal").on("click", function(){
 
 	});
 
+//initializes game start on load
 	resetGame();
+
 //closing tags for doc ready
 });
